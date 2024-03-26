d5e0f11d7600cca54b2f3f07f08d4aa1a86d11d6
UTHRATHITHAN <74050976+UTHRATHITHAN@users.noreply.github.com>
2024-03-26 11:56:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   env:{
+        GOOGLE_DRIVE_RESUME_LINK:"https://drive.google.com/file/d/1GZlVMBWfOQH_uIqRdo1SHJkD-Cd2QZBB/view"
+    },
    images:{
     remotePatterns:[
         {
